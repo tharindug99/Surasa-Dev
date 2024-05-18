6d4363ebda2f105a30a3745f51fdba1f46514bdd
tharindug99 <tngodage@std.appsc.sab.ac.lk>
2024-05-18 20:33:23 +0530

    Dashboard Recreated

--- frontend/routes/Routers.jsx
+++ frontend/routes/Routers.jsx
@@ -1,16 +1,17 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import {  Routes, Route } from 'react-router-dom';
-import Dashboard from '../src/pages/Dashbord/Dasboard.jsx';
+
 import App from '../src/App';
 import Order from '../src/pages/Order/Order.jsx';
+import Test from '../src/pages/Dashbord/Test.jsx';
 
 function Routers() {
   return (
     <Routes>
       <Route path="/" element={<App/>}></Route>
       <Route path="/order" element={<Order/>}></Route>
-      <Route path="/dashboard" element={<Dashboard/>}></Route>
+      <Route path="/test" element={<Test/>}></Route>
     </Routes>
   )
 }
---
+++ frontend/src/pages/Dashbord/components/LeftNavBar.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import {
+  BsCart3, BsGrid1X2Fill, BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill,
+  BsListCheck, BsMenuButtonWideFill, BsFillGearFill
+} from 'react-icons/bs'
+
+function LeftNavBar({ openSidebarToggle, OpenSidebar, setCurrentScreen }) {
+  return (
+    <aside className={`h-full bg-gray-800 overflow-y-auto transition-all duration-500 ${openSidebarToggle ? 'absolute z-20 w-64' : 'hidden md:block md:w-64'}`}>
+      <div className='flex justify-between items-center p-4 mb-8'>
+        <div className='flex items-center text-white'>
+          <BsCart3 className='text-2xl mr-2' /> 
+          <span className='font-bold text-lg'>SHOP</span>
+        </div>
+        <span className='text-red-500 text-lg cursor-pointer md:hidden' onClick={OpenSidebar}>X</span>
+      </div>
+
+      <ul className='list-none p-0'>
+        <li className='p-4 hover:bg-gray-700 cursor-pointer' onClick={() => setCurrentScreen('Home')}>
+          <a href="#" className='flex items-center text-white'>
+            <BsGrid1X2Fill className='text-xl mr-3' /> Home
+          </a>
+        </li>
+        <li className='p-4 hover:bg-gray-700 cursor-pointer' onClick={() => setCurrentScreen('Products')}>
+          <a href="#" className='flex items-center text-white'>
+            <BsFillArchiveFill className='text-xl mr-3' /> Products
+          </a>
+        </li>
+        <li className='p-4 hover:bg-gray-700 cursor-pointer' onClick={() => setCurrentScreen('BookingRequests')}>
+          <a href="#" className='flex items-center text-white'>
+            <BsFillGrid3X3GapFill className='text-xl mr-3' /> Booking Requests
+          </a>
+        </li>
+        <li className='p-4 hover:bg-gray-700 cursor-pointer' onClick={() => setCurrentScreen('Customers')}>
+          <a href="#" className='flex items-center text-white'>
+            <BsPeopleFill className='text-xl mr-3' /> Customers
+          </a>
+        </li>
+        <li className='p-4 hover:bg-gray-700 cursor-pointer' onClick={() => setCurrentScreen('Inventory')}>
+          <a href="#" className='flex items-center text-white'>
+            <BsListCheck className='text-xl mr-3' /> Inventory
+          </a>
+        </li>
+        <li className='p-4 hover:bg-gray-700 cursor-pointer' onClick={() => setCurrentScreen('Reports')}>
+          <a href="#" className='flex items-center text-white'>
+            <BsMenuButtonWideFill className='text-xl mr-3' /> Reports
+          </a>
+        </li>
+        <li className='p-4 hover:bg-gray-700 cursor-pointer' onClick={() => setCurrentScreen('SignOut')}>
+          <a href="#" className='flex items-center text-white'>
+            <BsFillGearFill className='text-xl mr-3' /> Sign Out
+          </a>
+        </li>
+      </ul>
+    </aside>
+  )
+}
+
+export default LeftNavBar
